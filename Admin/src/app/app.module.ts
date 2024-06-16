cb74002517f1c7fabf2b7ba7180b23dc4f6eee59
MarcioARA <marcio.correia@terra.com.br>
2024-06-15 22:27:27 -0300

    new projeto

---
+++ Admin/src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+// Page Route
+import { AppRoutingModule } from './app-routing.module';
+import { LayoutsModule } from './layouts/layouts.module';
+import { ToastrModule } from 'ngx-toastr';
+
+// Laguage
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+// component
+import { AppComponent } from './app.component';
+import { AuthlayoutComponent } from './authlayout/authlayout.component';
+
+export function createTranslateLoader(http: HttpClient): any {
+  return new TranslateHttpLoader(http, 'assets/i18n/', '.json');
+}
+
+@NgModule({ declarations: [
+        AppComponent,
+        AuthlayoutComponent
+    ],
+    bootstrap: [AppComponent], imports: [TranslateModule.forRoot({
+            defaultLanguage: 'en',
+            loader: {
+                provide: TranslateLoader,
+                useFactory: (createTranslateLoader),
+                deps: [HttpClient]
+            }
+        }),
+        BrowserModule,
+        BrowserAnimationsModule,
+        AppRoutingModule,
+        LayoutsModule,
+        ToastrModule.forRoot(),
+        FormsModule,
+        ReactiveFormsModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
+export class AppModule { }
